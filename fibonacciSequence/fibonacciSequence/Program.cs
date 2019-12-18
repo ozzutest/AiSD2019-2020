cc1855c3a7b2eb00b25c316b55120661018b85c5
ozzutest <150900@student.uwm.edu.pl>
2019-12-19 00:45:48 +0100

    Added fibonacci sequence (non-recursive, test)

---
+++ fibonacciSequence/fibonacciSequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibbonacci = new int[50];
+
+            fibbonacci[0] = 0;
+            fibbonacci[1] = 1;
+
+            for (int i = 2; i < fibbonacci.Length; i++)
+            {
+                fibbonacci[i] = fibbonacci[i - 2] + fibbonacci[i - 1];
+            }
+            
+            Console.WriteLine(string.Join(",\n", fibbonacci));
+
+            Console.ReadKey();
+
+        }
+    }
+}
